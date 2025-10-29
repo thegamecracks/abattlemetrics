35be5c6942314f127dc7d2a9610774b3e8f22794
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-10-29 00:22:24 -0400

    fix: explicitly re-export package members

--- src/abattlemetrics/__init__.py
+++ src/abattlemetrics/__init__.py
@@ -1,10 +1,21 @@
-from .client import *
-from .datapoint import *
-from .errors import *
-from .iterators import *
-from .player import *
-from .server import *
-from .session import *
+from .client import BattleMetricsClient as BattleMetricsClient
+from .datapoint import DataPoint as DataPoint, Resolution as Resolution
+from .errors import (
+    BattleMetricsException as BattleMetricsException,
+    HTTPException as HTTPException,
+)
+from .iterators import (
+    AsyncIterator as AsyncIterator,
+    AsyncPlayerListIterator as AsyncPlayerListIterator,
+    AsyncSessionIterator as AsyncSessionIterator,
+)
+from .player import (
+    IdentifierType as IdentifierType,
+    Identifier as Identifier,
+    Player as Player,
+)
+from .server import Server as Server
+from .session import Session as Session
 
 
 def _get_version() -> str:
